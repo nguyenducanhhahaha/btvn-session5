c9bae177fd8438d62efe6ccf3430d5d48f9ac87a
nguyenducanhhahaha <nguyenducanh@email.com>
2018-10-27 22:59:05 +0700

    2 cau dau

---
+++ nguyenducanh/numberstuff_intro.py
@@ -0,0 +1,9 @@
+yob = int(input("your year of birth??????"))
+age = 2018 - yob
+print(age)
+
+if yob >= 14:
+    print("Moi ban xem video")
+
+else:
+    print("Ban chua du trinh do de xem")
---
+++ nguyenducanh/weatherstuff_intro.py
@@ -0,0 +1,14 @@
+wmit = int(input("what month is this??????"))
+print(wmit)
+
+if wmit >= 10:
+    print("mua dong")
+
+elif wmit >= 9:
+    print('mua thu')
+
+elif wmit  >=6:
+    print("mua ha")
+
+else:
+    print("mua xuan")
